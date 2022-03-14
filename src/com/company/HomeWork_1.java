a24e7d5b89c30c90236186e8e582ae3087246f5f
MaciejKlos <maciej.klos071@gmail.com>
2022-03-14 23:32:03 +0100

    sample commit message

--- src/com/company/HomeWork_1.java
+++ src/com/company/HomeWork_1.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class HomeWork_1 {
-    // Napisz Program który prosi użytkownika o 2 liczbym a następnie wykona na nich podstawowe operacje matematyczne
+    //. Napisz Program który prosi użytkownika o 2 liczbym a następnie wykona na nich podstawowe operacje matematyczne
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj pierwszą liczbę");
