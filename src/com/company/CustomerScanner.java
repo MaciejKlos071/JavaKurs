ebf7855076ef1c58fb04493f3cca8948d00bf539
MaciejKlos <maciej.klos071@gmail.com>
2022-03-14 23:21:12 +0100

    zmiany

--- src/com/company/CustomerScanner.java
+++ src/com/company/CustomerScanner.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class CustomerScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoje imię: ");
         String name = scanner.nextLine();
-        System.out.println("Cześć" + name + "!");
-
+        System.out.println("Cześć" + name + " !");
+        
 
 
     }
