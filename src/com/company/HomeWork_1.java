31624769ce0bf786b8b942b485169f3ed1b00944
MaciejKlos <maciej.klos071@gmail.com>
2022-03-15 23:54:41 +0100

    sample commit message

--- src/com/company/HomeWork_1.java
+++ src/com/company/HomeWork_1.java
@@ -6,11 +6,24 @@ public class HomeWork_1 {
     //. Napisz Program który prosi użytkownika o 2 liczbym a następnie wykona na nich podstawowe operacje matematyczne
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double wynik;
         System.out.println("Podaj pierwszą liczbę");
-        int firstNumber = scanner.nextInt();
+        double firstNumber = scanner.nextInt();
         System.out.println("Podaj drugą liczbę");
-        int secoundNumber = scanner.nextInt();
+        double secoundNumber = scanner.nextInt();
         System.out.println("Pierwsza liczba: "+ firstNumber + "\nDruga liczba: "+ secoundNumber);
+
+        wynik = firstNumber + secoundNumber;
+        System.out.println("Suma: " + firstNumber + " + "+ secoundNumber +" = " + wynik);
+
+        wynik = firstNumber - secoundNumber;
+        System.out.println("Różnica: "+ firstNumber + " - "+ secoundNumber +" = " + wynik);
+        wynik = firstNumber / secoundNumber;
+        System.out.println("Iloczyn: "+ firstNumber + " / "+ secoundNumber +" = " + wynik);
+        wynik = firstNumber * secoundNumber;
+        System.out.println("iloraz: "+ firstNumber + " * "+ secoundNumber +" = " + wynik);
+        wynik = firstNumber % secoundNumber;
+        System.out.println("Modulo: "+ firstNumber + " % "+ secoundNumber +" = " + wynik);
 
 
     }
